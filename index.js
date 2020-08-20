cd1c6e3baa91498e6c4ac0853146667193713914
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-20 19:48:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined){
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    totalCost = total()
+    var totalCost = total()
     cart = [];
     return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
   }
