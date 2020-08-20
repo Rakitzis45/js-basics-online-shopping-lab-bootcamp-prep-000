2a780e4013dd1bc7ee79117abf83058016d6b93a
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-20 19:26:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ return total;
 function removeFromCart(item) {
  for (var i = 0; i < cart.length; i++){
 if (item === cart[i].itemName){
-   cart.splice(cart[i], 1);
+   cart.splice(i, 1);
    return cart
  }
  }
