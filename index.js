072eca2ec206fe89d12b8791f48fbe4bee28be1d
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-20 17:23:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var newItem = {
-    itemName: `${item}`,
-    itemPrice: Math.floor((Math.random()*100) + 1)
-  }
+  var newItem = {itemName: `${item}`, itemPrice: Math.floor((Math.random()*100) + 1)}
+  cart.push(newItem);
   return newItem.itemName + "has been added to your cart";
 console.log(itemPrice)
 }
