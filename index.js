2c6d4b694000f7f440e45dd61fe4f2e66a0a3042
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-20 18:46:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
   } else if (cart.length === 2){
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   } else {
-    for (i = 0; i < cart.length - 1; i++){
+    for (var i = 0; i < cart.length - 1; i++){
    var cartDescription = "In your cart, you have " ;
    cartDescription = cartDescription + `${cart[i].priceName} at $${cart[i].priceName}, `;
    return cartDescription + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
